47cee257fd3fdf33b58fad8ca157f7395148a3bb
webimpress <contact@webimpress.com>
2018-02-27 10:30:45 +0000

    ConfigProvider tests improvements

--- test/ConfigProviderTest.php
+++ test/ConfigProviderTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authorization-acl for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authorization-acl/blob/master/LICENSE.md New BSD License
  */
 
@@ -9,10 +9,14 @@ namespace ZendTest\Expressive\Authorization\Acl;
 
 use PHPUnit\Framework\TestCase;
 use Zend\Expressive\Authorization\Acl\ConfigProvider;
+use Zend\Expressive\Authorization\Acl\ZendAcl;
 
 class ConfigProviderTest extends TestCase
 {
-    public function setUp()
+    /** @var ConfigProvider */
+    private $provider;
+
+    protected function setUp()
     {
         $this->provider = new ConfigProvider();
     }
@@ -31,5 +35,10 @@ class ConfigProviderTest extends TestCase
     {
         $this->assertArrayHasKey('dependencies', $config);
         $this->assertInternalType('array', $config['dependencies']);
+        $this->assertArrayHasKey('factories', $config['dependencies']);
+
+        $factories = $config['dependencies']['factories'];
+        $this->assertInternalType('array', $factories);
+        $this->assertArrayHasKey(ZendAcl::class, $factories);
     }
 }
