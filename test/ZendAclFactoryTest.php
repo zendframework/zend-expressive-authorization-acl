98f25b1e769265b9ba5093638bafd9912f77b2e7
webimpress <contact@webimpress.com>
2018-02-27 10:31:57 +0000

    Added missing properties in tests

--- test/ZendAclFactoryTest.php
+++ test/ZendAclFactoryTest.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authorization-acl for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authorization-acl/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Expressive\Authorization\Acl;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Container\ContainerInterface;
 use Zend\Expressive\Authorization\Acl\ZendAcl;
 use Zend\Expressive\Authorization\Acl\ZendAclFactory;
@@ -15,6 +16,9 @@ use Zend\Expressive\Authorization\Exception;
 
 class ZendAclFactoryTest extends TestCase
 {
+    /** @var ContainerInterface|ObjectProphecy */
+    private $container;
+
     protected function setUp()
     {
         $this->container = $this->prophesize(ContainerInterface::class);
